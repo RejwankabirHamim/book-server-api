af80ec3911eed326a44fc98f4234b7c95baedb30
Hamim <hamim@appscode.com>
2024-07-15 16:06:19 +0600

    Remove unnecesary files & comments

--- cmd/start.go
+++ cmd/start.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -17,7 +14,6 @@ var (
 	password string
 )
 
-// startCmd represents the start command
 var startCmd = &cobra.Command{
 	Use:   "start",
 	Short: "This will start the server",
@@ -35,13 +31,5 @@ func init() {
 	startCmd.Flags().StringVarP(&port, "port", "p", "8080", "server port")
 	startCmd.Flags().StringVarP(&username, "username", "u", "hamim", "username")
 	startCmd.Flags().StringVarP(&password, "password", "s", "1234", "password")
-	// Here you will define your flags and configuration settings.
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// startCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
